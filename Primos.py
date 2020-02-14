2c7fa36b3c86e76e6aed3f53017a3df38a3fc57d
unknown <stephanods@hotmail.com>
2020-02-13 18:07:14 -0600

    Primos.py

---
+++ Primos.py
@@ -0,0 +1,3 @@
+n = 1000
+p = [2] + [x for x in range(3, n+1, 2) if not [y for y in range(3, int(x**0.5)+1, 2) if (float(x) / y).is_integer()]]
+print(p)
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Arquitectura-de-computadoras
 El repositorio de la clase de arquitectura de computadoras
+
+hola que pez
